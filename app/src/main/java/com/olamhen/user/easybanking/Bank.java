17e23898f9b9aec960c902d732037006940d877c
Abdullahi Usman <abdullahismn7@gmail.com>
2019-02-12 22:46:41 +0100

    feat: trying to convert all bank views to use one activity

---
+++ app/src/main/java/com/olamhen/user/easybanking/Bank.java
@@ -0,0 +1,23 @@
+package com.olamhen.user.easybanking;
+
+/**
+ * Created by dahham on 2/12/19.
+ * This file is part of EasyBanking2 licensed under GNU Public License
+ */
+public enum Banks {
+
+
+    ACCESS{
+        public final String BALANCE = "";
+        public final String HISTORY = "";
+    },
+    DIAMOND {
+
+    },
+    ECO{
+
+    }
+
+
+
+}
---
+++ app/src/main/java/com/olamhen/user/easybanking/BankTransactions.java
@@ -0,0 +1,8 @@
+package com.olamhen.user.easybanking;
+
+/**
+ * Created by dahham on 2/12/19.
+ * This file is part of EasyBanking2 licensed under GNU Public License
+ */
+public class BankTransactions {
+}
