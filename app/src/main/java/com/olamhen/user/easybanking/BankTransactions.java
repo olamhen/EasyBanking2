122bdfebfa0a613a0ad39f781790ecd0f8313b5a
Abdullahi Usman <abdullahismn7@gmail.com>
2019-02-12 22:49:23 +0100

    feat: bank activity page

--- app/src/main/java/com/olamhen/user/easybanking/BankTransactions.java
+++ app/src/main/java/com/olamhen/user/easybanking/BankTransactions.java
@@ -1,8 +1,25 @@
 package com.olamhen.user.easybanking;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by dahham on 2/12/19.
  * This file is part of EasyBanking2 licensed under GNU Public License
  */
-public class BankTransactions {
+public class BankTransactions extends AppCompatActivity {
+
+    Bank bank = null;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.acitvity_bank_transactions);
+
+        bank = (Bank) getIntent().getSerializableExtra("bank");
+
+
+        this.setTitle(bank.getName());
+    }
+
 }
